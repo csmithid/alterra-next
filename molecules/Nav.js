de02185956f8d0a147acb6f13e0e417dec8a08d7
Cole Smith <jeff.cole.smith@gmail.com>
2020-08-10 16:28:45 -0400

    nav animation

--- molecules/Nav.js
+++ molecules/Nav.js
@@ -1,16 +1,27 @@
 import Link from "next/link";
+import { useSpring, animated } from "react-spring";
+import { useRouter, Router } from "next/router";
+
+const NavButton = ({ href, children }) => {
+  const router = useRouter();
+  const props = useSpring({ opacity: router.pathname === href ? 1 : 0.5 });
+
+  return (
+    <Link href={href}>
+      <animated.a href="" style={props}>
+        {children}
+      </animated.a>
+    </Link>
+  );
+};
 
 const Nav = () => (
-  <nav className="flex flex-row gap-4 font-display font-light text-sm sm:text-lg">
-    <Link href="/">
-      <a>HOME</a>
-    </Link>
+  <nav className="flex flex-row gap-4 font-display font-light text-gray-800 text-md sm:text-lg">
+    <NavButton href="/">HOME</NavButton>
     <h2>•</h2>
-    <h2>PROJECTS</h2>
+    <NavButton href="/projects">PROJECTS</NavButton>
     <h2>•</h2>
-    <Link href="/about">
-      <a>ABOUT</a>
-    </Link>
+    <NavButton href="/about">ABOUT</NavButton>
   </nav>
 );
 
