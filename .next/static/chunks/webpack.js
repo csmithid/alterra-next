517189492939e135511c1e46fbd2f6c5a410943c
Cole Smith <jeff.cole.smith@gmail.com>
2020-08-10 16:31:56 -0400

    fix gaps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c1a303d7ec1e54de9380";
+/******/ 	var hotCurrentHash = "2443e38ff28bf164479d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- molecules/Motto.js
+++ molecules/Motto.js
@@ -1,9 +1,9 @@
 const Motto = () => (
-  <div className="font-light font-display my-12 text-gray-400 text-lg flex flex-col sm:flex-row items-center gap-2">
+  <div className="font-light font-display my-12 text-gray-400 text-lg flex flex-col sm:flex-row items-center">
     <h2>GENTES STRUIT</h2>
-    <h2 className="hidden sm:inline-block">•</h2>
+    <h2 className="hidden sm:inline-block mx-2">•</h2>
     <h2>NUBES CERNIT</h2>
-    <h2 className="hidden sm:inline-block">•</h2>
+    <h2 className="hidden sm:inline-block mx-2">•</h2>
     <h2>IGNES VINCIT</h2>
   </div>
 );
